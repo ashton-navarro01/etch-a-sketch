8b5decf51dd929c8e913c6ba0b990d82862da286
Ashton Navarro <ash.navarro425@gmail.com>
2025-11-18 18:28:50 +1100

    Added reset button and fixed styling of webpage to look cleaner

--- main.js
+++ main.js
@@ -10,11 +10,12 @@ function generateGrid(size){
         square.style.width = `${squareSize}px`;
 
         square.addEventListener('mouseenter', function() {
-            square.style.backgroundColor = "black"
-        })
-
-        square.addEventListener('mouseleave', function() {
-            square.style.backgroundColor = "white"
+            // Don't change colour if square is already coloured
+            if(square.dataset.hovered === "true") return;
+            
+            const color = getRandomColour();
+            square.style.backgroundColor = `${color}`
+            square.dataset.hovered = "true";
         })
 
         // Add the square to the container
@@ -22,19 +23,43 @@ function generateGrid(size){
     }
 }
 
-const sizeBtn = document.getElementById("change-size-btn");
-sizeBtn.addEventListener('click', function(){
-    while(true) {
-        let input = prompt("How big do you want the grid to be (under 100)?");
-        let size = Number(input);
-        if(input == null) return;
-        if(Number.isInteger(size) && size > 0 && size < 100) {
-            generateGrid(size);
-            return;
-        } else {
-            alert("Please enter a valid grid size.")
-        };
-    }
-})
+// Source - https://stackoverflow.com/a
+// Posted by AKX
+// Retrieved 2025-11-18, License - CC BY-SA 4.0
+function getRandomColour() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + "," + g + "," + b + ")";
+}
+
+
+function setupButtons(){
+    const sizeBtn = document.getElementById("change-size-btn");
+    sizeBtn.addEventListener('click', function(){
+        while(true) {
+            let input = prompt("How big do you want the grid to be (under 100)?");
+            let size = Number(input);
+            if(input == null) return;
+            if(Number.isInteger(size) && size > 0 && size < 100) {
+                generateGrid(size);
+                return;
+            } else {
+                alert("Please enter a valid grid size.")
+            };
+        }
+    })
+
+    const resetBtn = document.getElementById("reset-btn");
+    resetBtn.addEventListener('click', function(){
+        const squares = document.querySelectorAll(".grid-square")
+        squares.forEach(square => {
+            square.style.backgroundColor = "white";
+            square.dataset.hovered = "false";
+        })
+    });
+}
+
 
 generateGrid(16);
+setupButtons();
