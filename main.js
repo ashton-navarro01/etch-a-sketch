71d6dab916d1ae3ec9e094699a8ca164ac1d1ee8
Ashton Navarro <ash.navarro425@gmail.com>
2025-11-18 17:33:39 +1100

    Added feature of changing amount of squares on grid

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-function generateGrid(){
+function generateGrid(size){
     const container = document.getElementById("block-container");
-    for(let i = 0; i < 256; i++){
+    container.innerHTML = "";
+    const squareSize = 900 / size;
+    for(let i = 0; i < (size ** 2); i++){
         // Create the square and add css styling to it
         let square = document.createElement("div");
         square.classList.add("grid-square")
+
+        square.style.width = `${squareSize}px`;
 
         square.addEventListener('mouseenter', function() {
             square.style.backgroundColor = "red"
@@ -18,4 +22,18 @@ function generateGrid(){
     }
 }
 
-generateGrid();
+const sizeBtn = document.getElementById("change-size-btn");
+sizeBtn.addEventListener('click', function(){
+    while(true) {
+        let input = prompt("How big do you want the grid to be (under 100)?");
+        let size = Number(input);
+        if(Number.isInteger(size) && size > 0 && size < 100) {
+            generateGrid(size);
+            return;
+        } else {
+            alert("Please enter a valid grid size.")
+        };
+    }
+})
+
+generateGrid(16);
